31e21bc1273b03b53a8c3f923107ece0681b768b
Nikola Samardzic <nsamar@mit.edu>
2022-01-06 21:04:03 -0500

    Fix install script

--- essential-tools.sh
+++ essential-tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install sysstat bcc-tools linux-tools-common linux-tools-$(uname -r) iproute2 msr-tools
-sudo snap install bpftrace 
+sudo apt-get install sysstat bpfcc-tools linux-headers-$(uname -r) linux-tools-common linux-tools-$(uname -r) iproute2 msr-tools
+sudo snap install bpftrace
 git clone https://github.com/brendangregg/msr-cloud-tools
 git clone https://github.com/brendangregg/bpf-perf-tools-book
