bd1230b4f4a491fe2e969c642f778a34e62d01f4
Nikola Samardzic <nsamar@mit.edu>
2022-01-06 21:17:30 -0500

    Add tiptop installation

--- essential-tools.sh
+++ essential-tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install sysstat bpfcc-tools linux-headers-$(uname -r) linux-tools-common linux-tools-$(uname -r) iproute2 msr-tools
+sudo apt-get install tiptop sysstat bpfcc-tools linux-headers-$(uname -r) linux-tools-common linux-tools-$(uname -r) iproute2 msr-tools
 sudo snap install bpftrace
 git clone https://github.com/brendangregg/msr-cloud-tools
 git clone https://github.com/brendangregg/bpf-perf-tools-book
